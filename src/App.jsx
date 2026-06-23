3f83b9ae0b022708a5bbd3471f9c812dd42d9f75
zahrakaabi <kaabizahra@gmail.com>
2026-06-23 17:57:58 +0100

    chore: Implment project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>text</h1>
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'components'),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'constants'),
+      '#store': resolve(dirname(fileURLToPath(import.meta.url)), 'store'),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'hoc'),
+      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), 'windows')
+    }
+  }
+})
